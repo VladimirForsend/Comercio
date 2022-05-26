5233f6493021e7e668a4e2ea63ad56fedfc7a741
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-05-26 14:07:29 -0400

    Update comercio-color-icon.php

--- assets/customizer/comercio-color-icon.php
+++ assets/customizer/comercio-color-icon.php
@@ -1,38 +1,37 @@
 <?php
-add_action('customize_register', 'color_icono');
-function color_icono($wp_customize)
-{
+add_action( 'customize_register', 'color_icono' );
+function color_icono( $wp_customize ) {
 
 
     //seccion customizer
-    $wp_customize->add_section('color_icono', array(
+    $wp_customize->add_section( 'color_icono' , array(
         'title'      => 'Color íconos',
         'priority'   => 30,
-    ));
+    ) );
     //seccion customizer
 
-    $wp_customize->add_setting('color_iconos', array(
+    $wp_customize->add_setting( 'color_iconos' , array(
         'default'     => '#43C6E4',
         'transport'   => 'refresh',
-    ));
+    ) );
 
-    $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'color_iconos', array(
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'color_iconos', array(
         'label'        => 'Color íconos',
         'section'    => 'color_icono',
         'settings'   => 'color_iconos',
-    )));
-}
+    ) ) );
 
-add_action('wp_head', 'color_icono_titan');
+} 
+
+add_action( 'wp_head', 'color_icono_titan');
 function color_icono_titan()
 {
-?>
-    <style type="text/css">
-        .color_iconos {
-            color: <?php echo get_theme_mod('color_iconos', '#161616'); ?>;
-        }
-    </style>
-<?php
+    ?>
+         <style type="text/css">
+             .color_iconos{color:<?php echo get_theme_mod('color_iconos', '#161616'); ?>; }
+            
+         </style>
+    <?php
 }
 
 add_action('customize_register', 'color_titulos_comercio');
@@ -70,17 +69,16 @@ function color_titulos_comercio($wp_customize)
             )
         )
     );
-}
+} 
+ 
 
-
-add_action('wp_head', 'color_titulos_comercio');
+add_action( 'wp_head', 'color_titulosh1_comercio_titan');
 function color_titulosh1_comercio_titan()
 {
-?>
-    <style type="text/css">
-        body h1 {
-            color: <?php echo get_theme_mod('color_titulosh1_comercios', '#161616', '!important'); ?>;
-        }
-    </style>
-<?php
+    ?>
+         <style type="text/css">
+            body h1{color:<?php echo get_theme_mod('color_titulosh1_comercios', '#161616', '!important'); ?>; }
+            
+         </style>
+    <?php
 }
