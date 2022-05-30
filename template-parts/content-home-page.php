0c4a9e1a52b466662efcdb3378dfa2dcd6cd1327
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-05-30 19:04:27 -0400

    template content home page

---
+++ template-parts/content-home-page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template part for displaying page content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package comercio2
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
+	        <!-- slider de productos destacados -->
+			<?php include get_template_directory() . '/titan/productos/producto-destacados-post-type.php'; ?>
+        <!-- fin de slider de productos destacados -->
+		
+</article><!-- #post-<?php the_ID(); ?> -->
