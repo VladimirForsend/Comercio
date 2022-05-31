45c484eeea9cf5248c7ca686f624a13dff2b5de2
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-05-31 18:18:00 -0400

    Update nav-desk.php

--- assets/templates/navs/nav-desk.php
+++ assets/templates/navs/nav-desk.php
@@ -38,7 +38,7 @@
                         'theme_location' => 'menu-superior',
                         'menu_id'        => 'navbar',
                         'menu_class'     => 'navbar-nav me-auto mb-2 mb-lg-0',
-                        'container_class' => false,
+                        'container_class' => 'ul',
                     )
                 );
                 ?>
