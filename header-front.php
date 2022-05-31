d9eb0af81f0bc817e8a4592893b56c30ba43a678
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-05-31 14:58:03 -0400

    Quitando nav de tablet
    Quitando nav de tablet

--- header-front.php
+++ header-front.php
@@ -71,9 +71,6 @@
                             <?php if (function_exists('aws_get_search_form')) { aws_get_search_form(); } ?>
                         </div>
                     </ul>
-                    <div class="nav-tablet">
-                        hola
-                    </div>
 
                     <div class="d-flex justify-content-center align-items-center menu-cuenta-compras">
                         <?php
