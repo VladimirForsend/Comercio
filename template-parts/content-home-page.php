6609db069fbafe6dd1fdd4da20ee5d0199a7a686
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-05-30 20:05:20 -0400

    modulo productos
    modulo productos

--- template-parts/content-home-page.php
+++ template-parts/content-home-page.php
@@ -12,7 +12,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
 	        <!-- slider de productos destacados -->
-			<?php include get_template_directory() . '/titan/productos/producto-destacados-post-type.php'; ?>
+			<?php include get_template_directory() . '/assets/modulo-productos/producto-destacados-post-type.php'; ?>
         <!-- fin de slider de productos destacados -->
-		
+
 </article><!-- #post-<?php the_ID(); ?> -->
