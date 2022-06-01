55efb78dca6bc6399d8aab700e69e29f8b7dd7bc
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-06-01 17:05:45 -0400

    Update archive-product.php

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -29,7 +29,7 @@ get_header( 'shop' );
 do_action( 'woocommerce_before_main_content' );
 
 ?>
-hooolaaa prueba
+
 <header class="woocommerce-products-header">
 	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
@@ -45,6 +45,7 @@ hooolaaa prueba
 	do_action( 'woocommerce_archive_description' );
 	?>
 </header>
+hooolaaa prueba
 <?php
 if ( woocommerce_product_loop() ) {
 
