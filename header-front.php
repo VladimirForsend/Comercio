b71593df5ea74629b5671d64062611db62ce4e67
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-06-02 15:20:24 -0400

    cambios en el header

--- header-front.php
+++ header-front.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * The header for our theme 
+ * The header for our hola marcos 
  *
  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
