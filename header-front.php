39371cf11c8d611abac26fff69086dcc89d1b1d8
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-06-02 15:16:31 -0400

    Update header-front.php

--- header-front.php
+++ header-front.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * The header for our theme
+ * The header for our theme 
  *
  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
