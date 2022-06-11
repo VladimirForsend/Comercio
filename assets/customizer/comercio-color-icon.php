cb71af70fcfa2e03634cbccbb96120c8731f88eb
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-06-11 12:57:09 -0400

    Update comercio-color-icon.php

--- assets/customizer/comercio-color-icon.php
+++ assets/customizer/comercio-color-icon.php
@@ -17,7 +17,7 @@ function color_icono($wp_customize)
     ));
 
     $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'color_iconos', array(
-        'label'        => 'Color íconos',
+        'label'        => 'Color, íconos, botones, cajas',
         'section'    => 'color_icono',
         'settings'   => 'color_iconos',
     )));
