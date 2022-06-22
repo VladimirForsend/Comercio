23feaa1a70b8260d2940ca376f9a5ea4fb39a0f9
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-06-22 12:26:32 -0400

    Update slider.php

--- assets/modulos/modulo-slider/slider.php
+++ assets/modulos/modulo-slider/slider.php
@@ -1,8 +1,10 @@
 <!--Slider-->
 <script>
-   
-    $('head').append('<link rel="stylesheet" href="mstyle.css">');
-    
+    jQuery(function($) {
+
+
+        $('head').append('<link rel="stylesheet" href="mstyle.css">');
+    });
 </script>
 
 <div id="carouselExampleControls" class="carousel slide col-12" data-ride="carousel" data-interval="5000">
@@ -23,22 +25,26 @@
 
         if (have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
 
-                <div class="carousel-item <?php if ($active) { print("active"); }; ?>">
+                <div class="carousel-item <?php if ($active) {
+                                                print("active");
+                                            }; ?>">
                     <?php $active = false; ?>
-                    <a class="w-100" <?php $linkslide = get_field('link_del_banner'); if(!empty($linkslide)){?>href="<?php the_field('link_del_banner'); ?>"<?php }; ?>>
+                    <a class="w-100" <?php $linkslide = get_field('link_del_banner');
+                                        if (!empty($linkslide)) { ?>href="<?php the_field('link_del_banner'); ?>" <?php }; ?>>
                         <!--imagen mobile-->
-                        <?php $image = get_field('imagen_mobile'); $image2 = get_field('imagen_escritorio'); ?>
+                        <?php $image = get_field('imagen_mobile');
+                        $image2 = get_field('imagen_escritorio'); ?>
                         <?php if (!empty($image)) : ?>
-                            <img class="<?php if(!empty($image2)){ ?>d-sm-none<?php }; ?>" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                            <img class="<?php if (!empty($image2)) { ?>d-sm-none<?php }; ?>" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
                         <?php endif; ?>
 
                         <!--imagen escritorio-->
                         <?php if (!empty($image2)) : ?>
-                            <img class="<?php if(!empty($image)){ ?>d-none d-sm-block<?php }; ?>" src="<?php echo esc_url($image2['url']); ?>" alt="<?php echo esc_attr($image2['alt']); ?>" />
+                            <img class="<?php if (!empty($image)) { ?>d-none d-sm-block<?php }; ?>" src="<?php echo esc_url($image2['url']); ?>" alt="<?php echo esc_attr($image2['alt']); ?>" />
                         <?php endif; ?>
                     </a>
                 </div>
-                
+
             <?php endwhile; ?>
 
         <?php endif;
