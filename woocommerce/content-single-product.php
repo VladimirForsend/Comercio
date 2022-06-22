b58faa7c6b6ceb9fe41f721fc6e1e8fa50735580
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-06-22 17:54:32 -0400

    Update content-single-product.php

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -98,9 +98,9 @@ if (post_password_required()) {
                 <?php }; ?>
 
                 <div class="container">
-
+ 
                 <a class="whatsapp-productos" href="https://wa.me/56961591932?text=Me%20interesa%20Cotizar%20:%20'<?php echo get_the_title();?>'" target="_blank">
-                
+                 
                 <div class="d-none d-sm-flex producto-boton-wsapp"><i class="fab fa-whatsapp pr-2"></i>
                 
                 <div class="texto-wsapp">
