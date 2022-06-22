0247e99451f0f22991fbe7176dab72957e13f20e
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-06-22 16:52:56 -0400

    Update loop-infodelivery.php

--- assets/modulos/modulo-infodelivery/loop-infodelivery.php
+++ assets/modulos/modulo-infodelivery/loop-infodelivery.php
@@ -18,41 +18,25 @@
 			$wp_query = new WP_Query($args);
 			if (have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
 
-<div class="tipo--entrega">
-<!-- tipo de entrega nueva -->
-<?php $envio = get_field('envio_a_domicilio');
-$retiro = get_field('retira_tu_compra');
-if (!empty($envio) && !empty($retiro)) {
-} else { ?>
-	<div class="tipo-entrega-nueva">
-		<?php if (empty($envio)) { ?>
-			<div class="primera-parte">
-				<h5 class="">TODOS LOS ENVIOS SON POR PAGAR</h5>
-				<div class="">
-					<i class="fas fa-shipping-fast"></i>
-					<h6 class="">Zona Metropolitana de Santiago y Envío a regiones: </h6>
-					<p class="d-inline">Su pedido se despachara de 3 a 5 días hábiles después de efectuada la compra. Enviaremos a su correo el número de seguimiento dentro de las 24 horas.</p>
-				</div>
-			</div>
-		<?php }; ?>
-		<?php if (empty($retiro)) { ?>
-			<div class="segunda-parte">
-				<i class="fas fa-store-alt"></i>
-				<h6 class="">Compra directo en tienda, ¡Visítanos!</h6>
-				<ul class="">
-					<li class="">– San Alfonso 562 Metro Estación central</li>
-					<li class="">– Horario: Lunes a Viernes de 9:30 a 18:00 hrs. Sábado: 10:00 a 14:00 hrs.</li>
-				</ul>
-			</div>
-		<?php }; ?>
-	</div>
-<?php }; ?>
-<!-- fin de tipo de entrega nueva -->
-</div>
-</div>
-</div>
-</div>
-</div>
+					<div class="tipo--entrega">
+						<div class="primera-parte">
+							<h5 class="">TODOS LOS ENVIOS SON POR PAGAR</h5>
+							<div class="">
+								<i class="fas fa-shipping-fast"></i>
+								<h6 class="">Zona Metropolitana de Santiago y Envío a regiones: </h6>
+								<p class="d-inline">Su pedido se despachara de 3 a 5 días hábiles después de efectuada la compra. Enviaremos a su correo el número de seguimiento dentro de las 24 horas.</p>
+							</div>
+						</div>
+
+						<div class="segunda-parte">
+							<i class="fas fa-store-alt"></i>
+							<h6 class="">Compra directo en tienda, ¡Visítanos!</h6>
+							<ul class="">
+								<li class="">– San Alfonso 562 Metro Estación central</li>
+								<li class="">– Horario: Lunes a Viernes de 9:30 a 18:00 hrs. Sábado: 10:00 a 14:00 hrs.</li>
+							</ul>
+						</div>
+					</div>
 
 
 				<?php endwhile;
