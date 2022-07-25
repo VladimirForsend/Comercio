95ba0697e456de6879e4968a1b972341819928fb
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-07-25 14:59:45 -0400

    Update producto-post-type.php

--- assets/modulos/modulo-productos/producto-post-type.php
+++ assets/modulos/modulo-productos/producto-post-type.php
@@ -28,7 +28,7 @@
 
         ));
         ?>
-        <h3 class="col-12"><?php echo $terms ;?></h3>
+        <h3 class="col-12"><?php echo $terms_name ;?></h3>
         <?php
         wc_set_loop_prop('current_page', $paged);
         wc_set_loop_prop('is_paginated', wc_string_to_bool(true));
