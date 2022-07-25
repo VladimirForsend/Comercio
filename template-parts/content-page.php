dc5011b3ea14c6beeae59cc755522216b84e1790
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-07-25 18:38:01 -0400

    Update content-page.php

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -4,51 +4,22 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package comercio2
+ * @package ecommerce
  */
 
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
 
-	<?php comercio2_post_thumbnail(); ?>
+    <header class="entry-header" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>');">
+		<?php the_title( '<h1 class="titulo h1">', '</h1>' ); ?>
+	</header><!-- .entry-header --> 
 
-	<div class="entry-content">
-		<?php
-		the_content();
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'comercio2' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
+	<div id="ContenidoPage" class="entry-content container remove-col">
+		<?php the_content(); ?>
 	</div><!-- .entry-content -->
+    
+    <footer class="entry-footer">
+    </footer><!-- .entry-footer -->
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'comercio2' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
 </article><!-- #post-<?php the_ID(); ?> -->
